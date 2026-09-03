35ed9a915b1f8a5e3f91aae7962580ad75b02872
Akseli Kettunen <akseli.kettunen@helsinki.fi>
2026-09-03 21:54:45 +0300

    Set up project and basic express ping server.

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("beans");
+import express from "express";
+
+const app = express();
+
+app.get("/ping", (_req, res) => {
+  res.send("pong");
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`server listening on port ${PORT}`);
+});
